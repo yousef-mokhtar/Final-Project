744c903367ed2093c477d3e3131df5d28394ca9b
yousller <yousef.mokhtar1384@gmail.com>
2025-09-11 23:34:04 +0330

    Add Class Category in products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+class Product():
+    pass
