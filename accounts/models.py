de51d9bc34f3e79b77887e49aa14129b306acd5c
yousller <yousef.mokhtar1384@gmail.com>
2025-09-11 21:01:51 +0330

    Add class user in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import BaseModel
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser, BaseModel):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "phone"]
+
