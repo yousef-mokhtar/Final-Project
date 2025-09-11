2b3a43642c6f21bfe6045eea7163382262ea9c7e
yousller <yousef.mokhtar1384@gmail.com>
2025-09-11 22:55:14 +0330

    Add Class Store in seller/models.py

--- seller/models.py
+++ seller/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.models import BaseModel
+from accounts.models import User
 
-# Create your models here.
+class Store(BaseModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.TextField()
+    
