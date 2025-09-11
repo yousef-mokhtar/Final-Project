8a655cb3ab2aee28639b9db37b6de70d6c0c5332
yousller <yousef.mokhtar1384@gmail.com>
2025-09-11 04:40:03 +0330

    add core to installed_apps

--- customie_store/settings.py
+++ customie_store/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders.apps.OrdersConfig'
+    'orders.apps.OrdersConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
