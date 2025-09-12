3385404597b0e9e99ff494d025b51c0ddd61775f
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:41:32 +0330

    Add ProductAdmin Class

--- products/admin.py
+++ products/admin.py
@@ -6,5 +6,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'is_active')
     search_fields = ('name',)
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'created_at', 'is_active')
+    list_filter = ('category', 'brand',)
+    search_fields = ('name', 'brand')
+
+
+
+
 admin.site.register(ProductImage)
