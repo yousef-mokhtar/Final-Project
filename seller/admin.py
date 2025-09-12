c42a3bcb7e53ba712038b3444ed621f3bce06854
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:49:20 +0330

    Add StoreItemAdmin Class

--- seller/admin.py
+++ seller/admin.py
@@ -6,4 +6,8 @@ class StoreAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'created_at', 'is_active')
     search_fields = ('name', 'owner__email')
     
-admin.site.register(StoreItem)
+@admin.register(StoreItem)
+class StoreItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'store', 'product', 'price', 'stock', 'created_at', 'is_active')
+    list_filter = ('store', 'category')
+    search_fields = ('name', 'product__name', 'store__name')
