c04eedaeea0ecffc92528c2ff6ad7162c3a253c9
yousef <yousef.mokhtar1384@gmail.com>
2025-09-12 13:19:42 +0330

    Add more fields to Order Class

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from core.models import BaseModel
-from accounts.models import User
+from accounts.models import Address, User
+from products.models import Product
+from seller.models import StoreItem
 
 class Order(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     is_paid = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=15, default=0)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    paid_at = models.DateTimeField(null=True, blank=True)
