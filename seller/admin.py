02a0205f2715f14b7efc7c3cc47b717fd35570b0
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:47:14 +0330

    Add StoreAdmin Class

--- seller/admin.py
+++ seller/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Store, StoreItem
 
-admin.site.register(Store)
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at', 'is_active')
+    search_fields = ('name', 'owner__email')
+    
 admin.site.register(StoreItem)
