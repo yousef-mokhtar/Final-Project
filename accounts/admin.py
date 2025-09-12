6b10226e6dbc3de5718e5f491988c6190b61feb6
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:21:50 +0330

    Add UserAdmin Class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import User, Address, OTPCode
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'phone', 'is_verified', 'is_staff', 'is_active')
+    list_filter = ('is_verified', 'is_staff', 'is_active', 'groups')
+    search_fields = ('email', 'username', 'phone')
+    ordering = ('email',)
+
 admin.site.register(Address)
 admin.site.register(OTPCode)
