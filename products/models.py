54b970858baef3df551231a04c144f4dbf2e49d1
yousef <yousef.mokhtar1384@gmail.com>
2025-09-12 13:27:27 +0330

    Add Class Product in products/models.py

--- products/models.py
+++ products/models.py
@@ -6,5 +6,14 @@ class Category(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
 
-class Product():
-    pass
+class Product(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
