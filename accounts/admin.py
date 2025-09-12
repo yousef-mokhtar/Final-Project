2d0518c7346bb796d795975ccd834d95ce12f768
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:05:12 +0330

    Add User, Address, OTPCode to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Address, OTPCode
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(OTPCode)
