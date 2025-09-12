a7e8f21a8025e18ac4c9107cc2832cdcb5082c1e
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:32:51 +0330

    Add OrderAdmin Class

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'total_price', 'is_paid', 'paid_at', 'created_at')
+    list_filter = ('status', 'is_paid')
+    search_fields = ('user__email', 'id')
+    
+    
 admin.site.register(OrderItem)
