ad596162fc4da7e8486bb1452ab0a0f953ad0c1f
yousller <yousef.mokhtar1384@gmail.com>
2025-09-12 14:38:33 +0330

    Add CategoryAdmin Class and register

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product, ProductImage
+from .models import Category, Product, ProductImage
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'is_active')
+    search_fields = ('name',)
 
 admin.site.register(Product)
 admin.site.register(ProductImage)
