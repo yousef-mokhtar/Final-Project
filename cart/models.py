f84926c35187652cb6e426d51970ebc56b9f7060
yousller <yousef.mokhtar1384@gmail.com>
2025-09-16 18:42:44 +0330

    Transferring classes from payment to cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core.models import BaseModel
+from accounts.models import User
+from seller.models import StoreItem
 
-# Create your models here.
+class Cart(BaseModel):
+    user = models.OneToOneField(User, on_delete= models.CASCADE, related_name='cart')
+    is_active = models.BooleanField(default=True)
+    def total_price(self):
+        return sum([item.total_price for item in self.items.all()])
+    
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    store_item = models.ForeignKey(StoreItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'store_item')
+
+    @property
+    def total_price(self):
+        return self.quantity * self.store_item.price
+    
+    def __str__(self):
+        return f'{self.quantity} * {self.store_item.name}'
