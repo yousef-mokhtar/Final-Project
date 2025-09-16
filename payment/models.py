4cc3921c1a335ad1ca2d45de1bc3fd70801eb855
yousller <yousef.mokhtar1384@gmail.com>
2025-09-16 18:26:55 +0330

    Add CartItem Class

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from core.models import BaseModel
 from accounts.models import User
-from seller.models import Store
+from seller.models import StoreItem
 
 class Cart(BaseModel):
     user = models.OneToOneField(User, on_delete= models.CASCADE, related_name='cart')
@@ -9,3 +9,7 @@ class Cart(BaseModel):
     def total_price(self):
         return sum([item.total_price for item in self.items.all()])
     
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    store_item = models.ForeignKey(StoreItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
