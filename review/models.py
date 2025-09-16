c6ea4b91e65b60c623bc61e4b44a10fbbe5238d4
yousller <yousef.mokhtar1384@gmail.com>
2025-09-16 19:38:17 +0330

    Add Review Class

--- review/models.py
+++ review/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core.models import BaseModel
+from accounts.models import User
+from products.models import Product
 
-# Create your models here.
+class Review(BaseModel):
+    class Rating(models.IntegerChoices):
+        ONE = 1, "1"
+        TWO = 2, "2"
+        THREE = 3, "3"
+        FOUR = 4, "4"
+        FIVE = 5, "5"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveSmallIntegerField(choices=Rating.choices)
+    text = models.TextField(blank=True, null=True)
+    is_approved = models.BooleanField(default=False)
