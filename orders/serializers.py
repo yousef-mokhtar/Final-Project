c0014374c7c87e255b2b3a4b0aeca94c6513c67b
yousller <yousef.mokhtar1384@gmail.com>
2025-09-17 22:57:16 +0330

    validate address for OrderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,3 +6,10 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'user', 'shipping_address', 'total_price', 'status', 'is_paid', 'paid_at', 'items']
         read_only_fields = ['id', 'user', 'total_price', 'is_paid', 'paid_at']
+
+    def validate(self, data):
+        if not data.get('shipping_address'):
+            raise serializers.ValidationError('آدرس ارسال الزامی است.')
+        return data
+    
+    
