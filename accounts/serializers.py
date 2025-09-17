e00de5d7c78682c81933c7011944383e4364d837
yousller <yousef.mokhtar1384@gmail.com>
2025-09-17 17:19:32 +0330

    UserSerializer class

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,3 +4,20 @@ from rest_framework_simplejwt.serializers import TokenObtainSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'username', 'first_name', 'last_name']
+        read_only_fields = ['id']
+
+    email = serializers.EmailField(
+        error_messages={"unique": "این ایمیل قبلاً ثبت شده است."}
+    )
+
+    phone = serializers.CharField(
+        min_length=11,
+        max_length=11,
+        error_messages={
+            "min_length": "شماره تلفن باید 11 رقمی باشد.",
+            "max_length": "شماره تلفن باید 11 رقمی باشد.",
+            "unique": "این شماره تلفن قبلاً ثبت شده است.",
+        },
+    )
