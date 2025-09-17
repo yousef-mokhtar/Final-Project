b30ac6c9052288ede94907bc9c6128fbdd232590
yousller <yousef.mokhtar1384@gmail.com>
2025-09-17 22:48:21 +0330

    OrderSerializer class

---
+++ orders/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Order, OrderItem, Invoice
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'shipping_address', 'total_price', 'status', 'is_paid', 'paid_at', 'items']
+        read_only_fields = ['id', 'user', 'total_price', 'is_paid', 'paid_at']
