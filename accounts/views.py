80a39e239a8a6c541c80cad876c44077053b59cd
yousller <yousef.mokhtar1384@gmail.com>
2025-09-18 18:33:33 +0330

    Bae views.py accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .models import User, Address
+from .serializers import UserSerializer, AddressSerializer, OTPCodeSerilizer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class LoginView(TokenObtainPairView):
+    pass
+
+
+class TokenRefresh(TokenRefreshView):
+    pass
+
+class OTPRequestView(generics.GenericAPIView):
+    pass
+
+
+class UserProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    
