30fe8a09f41025c9f3697b32387a784b2149f86b
yousller <yousef.mokhtar1384@gmail.com>
2025-09-19 05:59:25 +0330

    AddToCartView to views.py

--- cart/views.py
+++ cart/views.py
@@ -13,3 +13,12 @@ class CartView(generics.RetrieveAPIView):
         # cart, _ = Cart.objects.get_or_create(user=self.request.user, is_active=True, is_deleted=False)
         # return cart
 
+class AddToCartView(generics.CreateAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serilizer):
+        from seller.models import StoreItem
+        store_item = StoreItem.objects.get(id=self.kwargs['store_item_id'], is_deleted=False)
+        cart = Cart.objects.get(user=self.request.user, is_active=True, is_deleted=False)
+        serilizer.save(cart=cart, store_item=store_item)
