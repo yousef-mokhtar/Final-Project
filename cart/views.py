6e085bb8089fb631670c376481de69cfa0fe22fd
yousller <yousef.mokhtar1384@gmail.com>
2025-09-19 04:54:13 +0330

    CartView to views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
 
-# Create your views here.
+class CartView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Cart.objects.get_or_create(user=self.request.user, is_active=True, is_deleted=False)[0]
+        # cart, _ = Cart.objects.get_or_create(user=self.request.user, is_active=True, is_deleted=False)
+        # return cart
+
