656254cb4a7859767884a5bfb6a0665a0b271182
yousller <yousef.mokhtar1384@gmail.com>
2025-09-22 16:18:00 +0330

    MyOrderView Added to orders view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Order, OrderItem, Invoice
+from .serializers import OrderSerializer, InvoiceSerializer
+from cart.models import Cart
 
-# Create your views here.
+class MyOrderView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user, is_deleted=False)
+    
