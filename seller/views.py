dd22ec2d319075d939b0498660094392bf5d4b4e
yousller <yousef.mokhtar1384@gmail.com>
2025-09-22 20:35:50 +0330

    Add RegisterAsSellerView

--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Store, StoreItem
+from serializers import StoreSerializer, StoreItemSerializer
 
-# Create your views here.
+class RegisterAsSellerView(generics.CreateAPIView):
+    serializer_class = StoreSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
