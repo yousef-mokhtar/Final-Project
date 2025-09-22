870ffb2f9a95d37ad55519745f10a77141fbee31
yousller <yousef.mokhtar1384@gmail.com>
2025-09-22 17:39:53 +0330

    Added AdminOrderViewSet for orders views

--- orders/views.py
+++ orders/views.py
@@ -5,13 +5,15 @@ from .models import Order, OrderItem, Invoice
 from .serializers import OrderSerializer, InvoiceSerializer
 from cart.models import Cart
 
+
 class MyOrderView(viewsets.ReadOnlyModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user, is_deleted=False)
-    
+
+
 class CheckoutView(generics.CreateAPIView):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
@@ -25,3 +27,13 @@ class CheckoutView(generics.CreateAPIView):
             Invoice.objects.create(order=order, user=self.request.user, invoices_number=f'INV-{order.id}', amount=order.total_price)
             cart.is_active = False
             cart.save()
+
+
+class AdminOrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.filter(is_deleted=False)
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminUser]
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
