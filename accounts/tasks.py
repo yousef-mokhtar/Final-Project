f6d21a2e8304e675aa479e3c1d0d9f3e4fa04a5e
yousller <yousef.mokhtar1384@gmail.com>
2025-09-26 14:48:45 +0330

    fixed send_otp_code

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,10 +1,17 @@
-from django.test import TestCase
 from celery import shared_task
-from accounts.models import OTPCode, User
-from django.utils import timezone
-from datetime import timedelta
+from django.core.mail import send_mail
+from accounts.models import User
 
 @shared_task
 def send_otp_code(user_id, code):
-    user = User.objects.get(id=user_id)
-    
+    try:
+        user = User.objects.get(id=user_id)
+        send_mail(
+            'کد OTP شما',
+            f'کد تأیید شما: {code}',
+            'from@test.com',
+            [user.email],
+            fail_silently=False,
+        )
+    except User.DoesNotExist:
+        pass  # لاگ خطا یا مدیریت خطا
