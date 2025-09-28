3951f88535777dc28a5996dd6b3b1c327a841e86
yousller <yousef.mokhtar1384@gmail.com>
2025-09-29 01:28:08 +0330

    celery.py for project

--- customie_store/celery.py
+++ customie_store/celery.py
@@ -1,8 +1,10 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customie_store.settings')
 
-app = Celery('your_project')
+app = Celery('customie_store')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 app.autodiscover_tasks()
