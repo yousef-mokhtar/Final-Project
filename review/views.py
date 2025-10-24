aeaf5db19cf7e8e9d3732f6cf5514e32e1386202
yousller <yousef.mokhtar1384@gmail.com>
2025-10-24 08:44:14 +0330

    ReviewViewSet for review

--- review/views.py
+++ review/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-from rest_framework import generics 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from products.models import Product
 from .models import Review
 from .serializers import ReviewSerializer
 
-class CreateReviewView(generics.CreateAPIView):
+class ReviewViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    - GET: لیست تمام نظرات برای یک محصول خاص را برمی‌گرداند.
+    - POST: یک نظر جدید برای یک محصول ثبت می‌کند.
+    """
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        product_id = self.kwargs.get('product_pk')
+        return Review.objects.filter(product_id=product_id, is_approved=True)
 
     def perform_create(self, serializer):
-        product_id = self.kwargs['id']
-        product = Product.objects.get(id=product_id, is_deleted=False)
+        product_id = self.kwargs.get('product_pk')
+        product = Product.objects.get(id=product_id)
         serializer.save(user=self.request.user, product=product)
