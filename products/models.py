a87c7ca22b42a957aef50dbd7ac60c60f6df7eb5
yousller <yousef.mokhtar1384@gmail.com>
2025-12-10 01:09:52 +0330

    edit product model

--- products/models.py
+++ products/models.py
@@ -6,6 +6,10 @@ class Category(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
